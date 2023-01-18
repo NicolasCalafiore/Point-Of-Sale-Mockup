38e7eb2ae99e90c46faf13067517c9e6931a609b
Nicolas Calafiore <121151093+NicolasCalafiore@users.noreply.github.com>
2023-01-18 15:31:14 -0800

    Update Program.cs

--- Delegates Practice/Program.cs
+++ Delegates Practice/Program.cs
@@ -12,18 +12,18 @@ namespace Delegates_Practice
         static bool isDebug;
         static void Main(string[] args)
         {
-            DisplayHandler displayHandler = new DisplayHandler();
-            ItemsManager inventoryManager = new ItemsManager();
-            Cart shoppingCart = new Cart();
-            bool isDebug = displayHandler.DisplayDebug();
+            DisplayHandler displayHandler = new DisplayHandler();                   // Handles What is Displayed on Console
+            ItemsManager inventoryManager = new ItemsManager();                     // Handles what items are abled to be purchased (add custom items here)
+            Cart shoppingCart = new Cart();                                         // Handles what the user chooses to purchase (handles price, tax status, etc.)
+            bool isDebug = displayHandler.DisplayDebug();                           // Enables Debug Outputs
 
 
 
 
-            Shopping shopping = new Shopping();
-            shopping.start(inventoryManager, displayHandler, shoppingCart);
-            Checkout checkout = new Checkout();
-            checkout.start(inventoryManager, shoppingCart, displayHandler, isDebug);
+            Shopping shopping = new Shopping();                                     // Creates new shopping simulation
+            shopping.start(inventoryManager, displayHandler, shoppingCart);         // Starts new shopping simulation (loop until head to checkout)
+            Checkout checkout = new Checkout();                                     // Starts new checkout simulation
+            checkout.start(inventoryManager, shoppingCart, displayHandler, isDebug);// Starts the checkout simulation (Payment Information & Processing)
         }
 
 
